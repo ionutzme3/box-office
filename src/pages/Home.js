e741992d932916933b80bb9a82ad67edff3ce0b7
Croitoru Ion <ionutzme3@gmail.com>
2020-09-17 02:22:32 +0300

    displayed show result and small fetch refactory

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
 import React, {useState} from "react";
 import MainPageLayout from "../components/MainPageLayout";
+import { apiGet } from "../misc/config";
 
 const Home = () => {
     const [input, setInput] = useState("");
+    const [results, setResults] = useState(null);
     const onSearch = () => {
-        fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-        .then(r => r.json())
-        .then(result => {
-            console.log(result);
+        apiGet(`/search/shows?q=${input}`).then(result => {
+            setResults(result);
         });
     };
     const onInputChange = ev => {
@@ -18,11 +18,26 @@ const Home = () => {
             onSearch();
         }
     };
-    
+    const renderResults = () => {
+        if (results && results.length === 0) {
+            return <div>No results</div>
+        }
+        if (results && results.length > 0){
+            return(
+                <div>
+                    {results.map(item => (
+                        <div key={item.show.id}>{item.show.name}</div>
+                    ))}
+                </div>
+            );
+        }
+        return null;
+    };
     return (
         <MainPageLayout>
             <input type="text" onChange={onInputChange} onKeyDown={onkeydown} value={input} />      
             <button type="button" onClick={onSearch} >Search</button>
+            {renderResults()}
         </MainPageLayout>
     );
 };
