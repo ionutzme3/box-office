2eafc8798526feec205a93942593e26601e3168d
Croitoru Ion <ionutzme3@gmail.com>
2020-09-17 00:49:28 +0300

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import {Switch, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div >
-      <h1>Hi</h1>
-    </div>
+    <Switch>
+      <Route exact path="/">This is home page</Route>
+      <Route exact path="/starred">This is starred</Route>
+      <Route>This is 404 page</Route>
+    </Switch>
   );
 }
 
