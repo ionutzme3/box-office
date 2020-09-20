89eb36558c79f4667a2125a632b60cef58e5e5bb
Croitoru Ion <ionutzme3@gmail.com>
2020-09-20 05:06:35 +0300

    Updates

---
+++ precache-manifest.9358e333a7dead378afccdb588ffea24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ae134f232969db900930e0ed42e725",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "7ec16afd46037f080579",
+    "url": "/box-office/static/css/main.3cd6ee85.chunk.css"
+  },
+  {
+    "revision": "f12df709ff9e5421da09",
+    "url": "/box-office/static/js/2.51fd0182.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.51fd0182.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ec16afd46037f080579",
+    "url": "/box-office/static/js/main.13257746.chunk.js"
+  },
+  {
+    "revision": "a24731daa79adae5d2cf",
+    "url": "/box-office/static/js/runtime-main.796231bd.js"
+  }
+]);
